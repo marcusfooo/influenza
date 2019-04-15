dd026c460beb3df08b1c219562919658776b3c74
Emil Luusua <emilluusua@gmail.com>
2019-04-15 15:23:57 +0800

    Changed file path

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -9,7 +9,7 @@ import numpy as np
 data_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
 data_path = './data/raw/'
 
-trigram_vecs, trigram_idxs = utils.read_and_process_to_trigram_vecs(data_files)
+trigram_vecs = utils.read_and_process_to_trigram_vecs(data_files, data_path)
 print(type(trigram_vecs[0][0]))
 print('Summed up to one vector per strain')
 print(f'Shape: {np.array(trigram_vecs).shape}')
