6e2bd303f166fc2d11ceb582a17ce72827b4c3bf
Emil Luusua <emilluusua@gmail.com>
2019-04-20 15:33:48 +0800

    Added validation when training

--- scripts/training.py
+++ scripts/training.py
@@ -1,5 +1,5 @@
 
-#%%
+#%% Creating training data
 from src.models import models, train_model
 from src.data import make_dataset
 from src.features import build_features
@@ -7,26 +7,45 @@ from src.features import original
 from src.utils import utils
 import torch
 import numpy as np
+import operator
+
+torch.manual_seed(0)
+np.random.seed(0)
 
 data_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
 data_path = './data/raw/'
 trigram_vecs, trigram_idxs = utils.read_and_process_to_trigram_vecs(data_files, data_path)
-
 labels = build_features.indexes_to_mutations(trigram_idxs[-2], trigram_idxs[-1])
-training_input = torch.FloatTensor(trigram_vecs[:-1])
-training_output = torch.LongTensor(labels)
 
-#%%
-torch.manual_seed(0)
-np.random.seed(0)
+trigram_vecs = np.array(trigram_vecs)
+labels = np.array(labels)
 
-input_dim = trigram_vecs_data.shape[1]
+num_of_training_examples = int(len(trigram_vecs[0]) // (5 / 4))
+
+train_vecs = trigram_vecs[:, :training_examples]
+train_labels = labels[:training_examples]
+test_vecs = trigram_vecs[:, training_examples:]
+test_labels = labels[training_examples:]
+
+X_train = torch.FloatTensor(train_vecs)
+Y_train = torch.LongTensor(train_labels)
+X_test = torch.FloatTensor(test_vecs)
+Y_test = torch.LongTensor(test_labels)
+
+#%% Training model
+_, counts = np.unique(Y_test, return_counts=True)
+majority_acc = max(counts) / Y_test.shape[0]
+print('Accuracy for majority vote: %.4f' % majority_acc)
+
+input_dim = X_train.shape[2]
 output_dim = 2
 hidden_size = 400
 num_of_layers = 1
-
 net = models.LSTMModel(input_dim, hidden_size, num_of_layers, output_dim)
-train_model.train_rnn(net, 5, 0.1, 256, training_input, training_output)
 
+num_of_epochs = 3
+learning_rate = 0.1
+batch_size = 256
+train_model.train_rnn(net, num_of_epochs, learning_rate, batch_size, X_train, Y_train, X_test, Y_test)
 
 #%%
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn.functional as F
 import math
 import time
+import matplotlib.pyplot as plt
 
 def repackage_hidden(h):
         """Wraps hidden states in new Tensors, to detach them from their history."""
@@ -9,16 +11,22 @@ def repackage_hidden(h):
         else:
             return tuple(repackage_hidden(v) for v in h)
 
-def train_rnn(model,epochs, learning_rate, batch_size, X, Y):
+def train_rnn(model, epochs, learning_rate, batch_size, X, Y, X_test, Y_test):
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    loss_function = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.CrossEntropyLoss()
     num_of_examples = X.shape[1]
     num_of_batches = math.floor(num_of_examples/batch_size)
+
+    all_losses = []
+    all_val_losses = []
+    all_accs = []
+    all_val_accs = []
 
     start_time = time.time()
     for epoch in range(epochs):
         running_loss = 0
+        running_acc = 0
 
         hidden = model.init_hidden(batch_size)
 
@@ -30,15 +38,49 @@ def train_rnn(model,epochs, learning_rate, batch_size, X, Y):
             Y_batch = Y[count:count+batch_size]
 
             scores = model(X_batch, hidden)
-            loss = loss_function(scores, Y_batch)
-
+            loss = criterion(scores, Y_batch)
             loss.backward()
             optimizer.step()
 
+            prob = F.softmax(scores, dim=1)
+            _, prediction = torch.topk(prob, 1)
+            prediction = prediction.view_as(Y_batch)
+            running_acc += int((Y_batch.eq(prediction)).sum())
+
             running_loss += loss.item()
 
-        epoch_loss = running_loss/num_of_batches
-        elapsed_time = time.time()-start_time
+        elapsed_time = time.time() - start_time
+        epoch_acc = running_acc / Y.shape[0]
+        all_accs.append(epoch_acc)
+        epoch_loss = running_loss / num_of_batches
+        all_losses.append(epoch_loss)
 
+        with torch.no_grad():
+            scores = model(X_test, model.init_hidden(Y_test.shape[0]))
+            prob = F.softmax(scores, dim=1)
+            _, prediction = torch.topk(prob, 1)
+            prediction = prediction.view_as(Y_test)
 
-        print('epoch=', epoch, '\t time=', elapsed_time, '\t loss',  epoch_loss)
+            correct = (Y_test.eq(prediction)).sum()
+            val_acc = int(correct)/Y_test.shape[0]
+            all_val_accs.append(val_acc)
+
+            val_loss = criterion(scores, Y_test).item()
+            all_val_losses.append(val_loss)
+
+        print('Epoch: %d\tTime: %.1f s\tLoss: %.4f\tAcc: %.4f\tVal loss: %.4f\tVal acc: %.4f'
+            % (epoch, elapsed_time, epoch_loss, epoch_acc, val_loss, val_acc))
+        
+    plt.style.use('ggplot')
+    plt.figure()
+    plt.plot(all_losses, 'b', label='Training')
+    plt.plot(all_val_losses, 'r', label='Validation')
+    plt.title('Loss')
+    plt.legend()
+
+    plt.figure()
+    plt.plot(all_accs, 'b', label='Training')
+    plt.plot(all_val_accs, 'r', label='Validation')
+    plt.title('Accuracy')
+    plt.legend()
+    plt.show()
