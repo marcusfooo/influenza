2b433987af623d0d78c22a1dbb8a5f6bd3c8d3f8
Emil Luusua <emilluusua@gmail.com>
2019-04-21 19:27:48 +0800

    Training and utils.read_and.. now splits data into train and test sets

--- scripts/training.py
+++ scripts/training.py
@@ -9,27 +9,21 @@ import torch
 import numpy as np
 import operator
 
-torch.manual_seed(0)
-np.random.seed(0)
+torch.manual_seed(1)
+np.random.seed(1)
 
-data_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
 data_path = './data/raw/'
-trigram_vecs, trigram_idxs = utils.read_and_process_to_trigram_vecs(data_files, data_path)
-labels = build_features.indexes_to_mutations(trigram_idxs[-2], trigram_idxs[-1])
+train_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
+train_trigram_vecs, test_trigram_vecs, train_trigram_idxs, test_trigram_idxs = utils.read_and_process_to_trigram_vecs(train_files, data_path, sample_size=625, test_split=0.2)
 
-trigram_vecs = np.array(trigram_vecs)
-labels = np.array(labels)
+train_trigram_vecs = np.array(train_trigram_vecs)
+test_trigram_vecs = np.array(test_trigram_vecs)
+train_labels = build_features.indexes_to_mutations(train_trigram_idxs[-2], train_trigram_idxs[-1])
+test_labels = build_features.indexes_to_mutations(test_trigram_idxs[-2], test_trigram_idxs[-1])
 
-num_of_training_examples = int(len(trigram_vecs[0]) // (5 / 4))
-
-train_vecs = trigram_vecs[:, :training_examples]
-train_labels = labels[:training_examples]
-test_vecs = trigram_vecs[:, training_examples:]
-test_labels = labels[training_examples:]
-
-X_train = torch.FloatTensor(train_vecs)
+X_train = torch.FloatTensor(train_trigram_vecs)
 Y_train = torch.LongTensor(train_labels)
-X_test = torch.FloatTensor(test_vecs)
+X_test = torch.FloatTensor(test_trigram_vecs)
 Y_test = torch.LongTensor(test_labels)
 
 #%% Training model
@@ -39,11 +33,11 @@ print('Accuracy for majority vote: %.4f' % majority_acc)
 
 input_dim = X_train.shape[2]
 output_dim = 2
-hidden_size = 400
+hidden_size = 10
 num_of_layers = 1
 net = models.LSTMModel(input_dim, hidden_size, num_of_layers, output_dim)
 
-num_of_epochs = 3
+num_of_epochs = 500
 learning_rate = 0.1
 batch_size = 256
 train_model.train_rnn(net, num_of_epochs, learning_rate, batch_size, X_train, Y_train, X_test, Y_test)
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,24 +1,36 @@
 from src.data import make_dataset
 from src.features import build_features
+import math
 
-def read_and_process_to_trigram_vecs(data_files, data_path='../data/raw/', sample_size=100, concat=True):
+def read_and_process_to_trigram_vecs(data_files, data_path='../data/raw/', sample_size=100, test_split=0.0, concat=True):
   trigram_to_idx, trigram_vecs_data = make_dataset.read_trigram_vecs(data_path)
 
   strains_by_year = make_dataset.read_strains_from(data_files, data_path)
 
-  if sample_size > 0:
-    strains_by_year = build_features.sample_strains(strains_by_year, sample_size)
+  train_strains_by_year, test_strains_by_year = make_dataset.train_test_split_strains(strains_by_year, test_split)
+  training_samples = int(math.floor(sample_size * (1 - test_split)))
+  test_samples = sample_size - training_samples
 
-  trigrams_by_year = build_features.split_to_trigrams(strains_by_year)
+  if training_samples > 0:
+    train_strains_by_year = build_features.sample_strains(train_strains_by_year, training_samples)
+  if test_samples > 0:
+    test_strains_by_year = build_features.sample_strains(test_strains_by_year, test_samples)
 
-  trigram_idxs = build_features.trigrams_to_indexes(trigrams_by_year, trigram_to_idx)
+  train_trigrams_by_year = build_features.split_to_trigrams(train_strains_by_year)
+  test_trigrams_by_year = build_features.split_to_trigrams(test_strains_by_year)
 
+  train_trigram_idxs = build_features.trigrams_to_indexes(train_trigrams_by_year, trigram_to_idx)
+  test_trigram_idxs = build_features.trigrams_to_indexes(test_trigrams_by_year, trigram_to_idx)
+  
   if concat:
-    trigram_idxs = build_features.concat_trigrams(trigram_idxs)
-    trigram_vecs = build_features.indexes_to_trigram_vecs(trigram_idxs, trigram_vecs_data)
+    train_trigram_idxs = build_features.concat_trigrams(train_trigram_idxs)
+    train_trigram_vecs = build_features.indexes_to_trigram_vecs(train_trigram_idxs, trigram_vecs_data)
+    test_trigram_idxs = build_features.concat_trigrams(test_trigram_idxs)
+    test_trigram_vecs = build_features.indexes_to_trigram_vecs(test_trigram_idxs, trigram_vecs_data)
   else:
-    trigram_vecs = build_features.indexes_by_year_to_trigram_vecs(trigram_idxs, trigram_vecs_data)
-
-  return trigram_vecs, trigram_idxs
+    train_trigram_vecs = build_features.indexes_by_year_to_trigram_vecs(train_trigram_idxs, trigram_vecs_data)
+    test_trigram_vecs = build_features.indexes_by_year_to_trigram_vecs(test_trigram_idxs, trigram_vecs_data)
+  
+  return train_trigram_vecs, test_trigram_vecs, train_trigram_idxs, test_trigram_idxs
 
 
