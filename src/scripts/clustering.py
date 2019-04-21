c429ef42a89ea4095b1335f7e27c1fd0b18e6f14
Jan Dabrowski <dabrowski.janek@gmail.com>
2019-04-22 01:37:34 +0800

    save clustering results

--- src/scripts/clustering.py
+++ src/scripts/clustering.py
@@ -1,15 +1,17 @@
 from src.utils import utils
 from src.data import cluster
 from src.visualization import visualize
+from src.data import make_dataset
+import pandas as pd
 
 # data_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
-data_files = ['2014.csv', '2015.csv', '2016.csv']
+data_files = ['2013.csv', '2014.csv', '2015.csv', '2016.csv']
 data_path = './data/raw/'
 
 methods = ['DBSCAN', 'KMeans', 'MeanShift']
 methods = ['DBSCAN']
 
-trigram_vecs, _ = utils.read_and_process_to_trigram_vecs(data_files, data_path, sample_size=0, concat=False)
+trigram_vecs, _, _, _ = utils.read_and_process_to_trigram_vecs(data_files, data_path, sample_size=0, concat=False)
 
 # concated_years = [[]]
 # for year_trigram_vecs in trigram_vecs:
@@ -19,11 +21,25 @@ trigram_vecs, _ = utils.read_and_process_to_trigram_vecs(data_files, data_path, 
 print(f'Shape: {len(trigram_vecs)}x{len(trigram_vecs[0])}x{len(trigram_vecs[0][0])}')
 
 for method in methods:
-    clusters = cluster.cluster_years(trigram_vecs, method=method)
-    print(f'Number of clusters in first year: {len(clusters[0][2])}')
-    average = cluster.evaluate_clusters(clusters)
-    print(f'Average score of {method}: {average}')
+    clusters_by_year = cluster.cluster_years(trigram_vecs, method)
+    print(f'Number of clusters in the first year: {len(clusters_by_year[0]["centroids"])}')
+    average = cluster.evaluate_clusters(clusters_by_year)
+    print(f'Average variance of {method}: {average}')
 
-    linked_clusters = cluster.link_clusters(clusters)
+    # linked_clusters = cluster.link_clusters(clusters_by_year)
+    # print(linked_clusters)
+
+    strains_by_year = make_dataset.read_strains_from(data_files, data_path)
+    for i, clusters in enumerate(clusters_by_year):
+        path = data_files[i]
+
+        df = pd.read_csv(data_path + path)
+        df['cluster'] = clusters['labels']
+        for i, centroid in enumerate(clusters['centroids']):
+            df[str(i)] = pd.Series(centroid)
+
+        path = '../data/interim/'+path
+        print(f'saving to : {path}')
+        df.to_csv(path)
 
     # visualize.show_clusters(clusters, method='TSNE')
