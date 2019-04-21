6ff007846f751fd5a7e0e353bd366ad73e9c2787
Jan Dabrowski <dabrowski.janek@gmail.com>
2019-04-21 23:00:52 +0800

    Add dbscan and meanshift clustering

--- src/data/cluster.py
+++ src/data/cluster.py
@@ -1,23 +1,43 @@
 from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
-import numpy as np
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.neighbors import NearestNeighbors
 
-def cluster_years(trigram_vecs, method='DBSCAN'):
+import numpy as np
+from math import floor
+
+def cluster_years(trigram_vecs, method='DBSCAN', remove_outliers=True):
     clusters = []
     for year_trigram_vecs in trigram_vecs:
-        prot_vecs = np.array(year_trigram_vecs).sum(axis=2)
+        prot_vecs = np.array(year_trigram_vecs).sum(axis=1)
 
         if(method == 'DBSCAN'):
-            clf = DBSCAN(eps=0.01, metric='cosine').fit(prot_vecs)
+            min_samples = floor(len(year_trigram_vecs)*0.01)
+            clf = DBSCAN(eps=5, min_samples=min_samples, metric='euclidean').fit(prot_vecs)
             labels = clf.labels_
+            centroids = NearestCentroid().fit(prot_vecs, labels).centroids_
+
+        if(method == 'MeanShift'):
+            clf = MeanShift().fit(prot_vecs)
+            labels = clf.labels_
+            centroids = clf.cluster_centers_
 
         if(method == 'KMeans'):
             clf = KMeans(n_clusters=3)
             clf.fit(prot_vecs)
             labels = clf.labels_
+            centroids = clf.cluster_centers_
 
-        clusters.append([prot_vecs, labels])
+        if(remove_outliers):
+            idxs_to_remove = []
+            for i, label in enumerate(labels):
+                if(label == -1): idxs_to_remove.append(i)
+            prot_vecs = [prot_vec for i, prot_vec in enumerate(prot_vecs) if i not in idxs_to_remove]
+            labels = [label for i, label in enumerate(labels) if i not in idxs_to_remove]
+
+        clusters.append([prot_vecs, labels, centroids])
 
     return clusters
 
@@ -31,4 +51,26 @@ def evaluate_clusters(clusters):
         scores.append(score)
 
     average = sum(scores) / float(len(scores))
-    print(f'Average score: {average}')
+    return average
+
+def link_clusters(clusters):
+    linked = []
+    no_years = len(clusters)
+
+    first_cluster = clusters[0]
+    centroids = first_cluster[2]
+
+    neigh = NearestNeighbors(n_neighbors=2)
+
+    for centroid in centroids:
+        links = []
+        current_centroid = centroid
+        for year_idx in range(no_years-1):
+            year_centroids = clusters[year_idx+1][2]
+            neigh.fit(year_centroids)
+
+            ind = neigh.kneighbors([current_centroid], return_distance=False)
+            current_centroid = year_centroids[ind[0][0]]
+            links.append(ind)
+        
+        print(links)
--- src/scripts/clustering.py
+++ src/scripts/clustering.py
@@ -1,23 +1,29 @@
-#%%
-# get_ipython().run_line_magic('load_ext', 'autoreload')
-# get_ipython().run_line_magic('autoreload', '2')
-
-#%%
 from src.utils import utils
 from src.data import cluster
 from src.visualization import visualize
 
-data_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
-data_files = ['2015.csv', '2016.csv']
-data_files = ['2015.csv']
+# data_files = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv']
+data_files = ['2014.csv', '2015.csv', '2016.csv']
 data_path = './data/raw/'
 
+methods = ['DBSCAN', 'KMeans', 'MeanShift']
+methods = ['DBSCAN']
+
 trigram_vecs, _ = utils.read_and_process_to_trigram_vecs(data_files, data_path, sample_size=0, concat=False)
+
+# concated_years = [[]]
+# for year_trigram_vecs in trigram_vecs:
+#     concated_years[0] += year_trigram_vecs
+# trigram_vecs = concated_years
+
 print(f'Shape: {len(trigram_vecs)}x{len(trigram_vecs[0])}x{len(trigram_vecs[0][0])}')
 
-clusters = cluster.cluster_years(trigram_vecs, method='KMeans')
-average = cluster.evaluate_clusters(clusters)
+for method in methods:
+    clusters = cluster.cluster_years(trigram_vecs, method=method)
+    print(f'Number of clusters in first year: {len(clusters[0][2])}')
+    average = cluster.evaluate_clusters(clusters)
+    print(f'Average score of {method}: {average}')
 
-clusters = cluster.cluster_years(trigram_vecs, method='DBSCAN')
-cluster.evaluate_clusters(clusters)
-# visualize.show_clusters(clusters, method='PCA')
+    linked_clusters = cluster.link_clusters(clusters)
+
+    # visualize.show_clusters(clusters, method='TSNE')
