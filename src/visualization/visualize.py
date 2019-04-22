41155ccceab13c551f746ace4f865050c6544412
Jan Dabrowski <dabrowski.janek@gmail.com>
2019-04-22 13:25:48 +0800

    visualize with 3 dimentions

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import numpy as np
 
 def show_clusters(clusters, dims=2, method='TSNE'):
     for cluster in clusters:
-        prot_vecs = cluster[0]
-        labels = cluster[1]
+        prot_vecs = cluster['prot_vecs']
+        labels = cluster['labels']
 
         print(f'Shape: {np.array(prot_vecs).shape}')
 
@@ -18,8 +19,12 @@ def show_clusters(clusters, dims=2, method='TSNE'):
             reduced_data = PCA(n_components=dims).fit_transform(prot_vecs)
 
         colors = 10 * ['r.', 'g.', 'y.', 'c.', 'm.', 'b.', 'k.']
-        plt.figure()
+        fig = plt.figure()
         for i in range(len(reduced_data)):
-            plt.plot(reduced_data[i][0], reduced_data[i][1], colors[labels[i]], markersize=10)
+            if (dims == 2):
+                plt.plot(reduced_data[i][0], reduced_data[i][1], colors[labels[i]], markersize=10)
+        if (dims == 3):
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(reduced_data[:][0], reduced_data[:][1], reduced_data[:][2], colors[labels[i]], marker='o')
 
     plt.show()
