d9252ee2e362c9f9990a45b77f3b6e6950c5c57e
Emil Luusua <emilluusua@gmail.com>
2019-04-22 13:27:40 +0800

    add first version of attention model

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,5 @@
 
-from torch import nn, zeros
+from torch import nn, zeros, bmm
 import torch.nn.functional as F
 
 class LSTMModel(nn.Module):
@@ -30,28 +30,37 @@ class LSTMModel(nn.Module):
 
 class AttentionModel(nn.Module):
   """TODO: DOCSTRING"""
-  def __init__(self, input_dim, hidden_size, attention_size, num_of_layers, output_dim):
+  def __init__(self, input_dim, seq_length, hidden_size, num_of_layers, output_dim):
     super(AttentionModel, self).__init__()
 
     self.hidden_size = hidden_size
+    self.seq_length = seq_length
     self.num_of_layers = num_of_layers
     
-    self.layer1 = nn.LSTM(input_dim, hidden_size, num_of_layers)
-    self.att_w1 = nn.Linear(hidden_size, attention_size)
-    self.att_w2 = nn.Linear(attention_size, attention_size)
-    self.att_v = nn.Linear(attention_size, 1)
-    self.fc_layer = nn.Linear(hidden_size, output_dim)
+    self.encoder = nn.LSTM(input_dim, hidden_size, num_of_layers)
+    self.attn = nn.Linear(hidden_size, seq_length)
+    self.out = nn.Linear(hidden_size, output_dim)
+
 
   def forward(self, input_seq, hidden_state):
-    out, h, c = self.layer1(input_seq, hidden_state)
-    x = self.attention(out, h)
+    encoder_outputs, (h, _) = self.encoder(input_seq, hidden_state)
+    attn_applied, attn_weights = self.attention(encoder_outputs, h)
+    score_seq = self.out(attn_applied.view(-1, self.hidden_size))
+
+    return score_seq
 
 
-  def attention(self, features, h):
-    score = F.tanh(self.att_w1(features) + self.att_w2(h))
-    attention_weights = nn.functional.softmax(self.att_v(score), dim=1)
-    context_vec = attention_weights * features
-    #context_vec = context_vec.sum()
-    #context_vec = tf.reduce_sum(context_vector, axis=1)
+  def attention(self, encoder_outputs, hidden):
+    attn_weights = F.softmax(self.attn(hidden[-1]), dim=1)
+    attn_weights = attn_weights.view(-1, 1, self.seq_length)
+    encoder_outputs = encoder_outputs.view(-1, self.seq_length, self.hidden_size)
+    attn_applied = bmm(attn_weights, encoder_outputs)
 
-    return context_vec, attention_weights
+    return attn_applied, attn_weights
+
+
+  def init_hidden(self, batch_size):
+    h_init = zeros(self.num_of_layers, batch_size, self.hidden_size)
+    c_init = zeros(self.num_of_layers, batch_size, self.hidden_size)
+    
+    return (h_init, c_init)
